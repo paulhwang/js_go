5685db24987bb8e5ee37b658ee21aeb56d60f96f
paul.hwang (黃文樸 - Getac) <paul.hwang@getac.com.tw>
2016-11-19 10:52:35 +0800

    ajax

--- js/util_folder/util_object.js
+++ js/util_folder/util_object.js
@@ -9,8 +9,8 @@ var LOG_IT = function (str1_val, str2_val) {
 };
 
 var ABEND = function (str1_val, str2_val) {
-    window.console.log("abend: " + str1_val + "() " + str2_val);
-    window.alert("abend: " + str1_val + "() " + str2_val);
+    window.console.log("***ABEND*** " + str1_val + "() " + str2_val);
+    window.alert("***ABEND*** " + str1_val + "() " + str2_val);
     var x = junk;
 };
 
