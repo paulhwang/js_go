d49c804db0bf6f570469f2eaec706e72eab32d2c
Paul Hwang <paul.hwang@getac.com.tw>
2016-12-07 13:30:29 +0800

    switch

--- js/fabric_folder/switch_object.js
+++ js/fabric_folder/switch_object.js
@@ -38,83 +38,9 @@ function SwitchObject(root_object_val) {
     };
 
     this.setupLinkResponse = function (input_val) {
-        this.debug_(true, this.debugInput(), "setupLinkResponse", "input_val=" + input_val);
+        this.debug(true, "setupLinkResponse", "input_val=" + input_val);
         var data = JSON.parse(input_val);
         this.rootObject().mallocLinkObject(data.my_name, data.link_id);
-    };
-
-    this.getLinkDataResponse = function (input_val) {
-        this.debug_(false, this.debugInput(), "getLinkDataResponse", "input_val=" + input_val);
-        var data = JSON.parse(input_val);
-        if (data) {
-            if (this.linkObject().linkId() !== data.link_id) {
-                this.abend("getLinkDataResponse", "linkId=" + data.link_id);
-                return;
-            }
-            this.linkObject().getLinkDataResponse(input_val);
-        }
-    };
-
-    this.getNameListResponse = function (input_val) {
-        this.debug_(true, this.debugInput(), "getNameListResponse", "input_val=" + input_val);
-        var data = JSON.parse(input_val);
-        if (data) {
-            if (this.linkObject().linkId() !== data.link_id) {
-                this.abend("getNameListResponse", "linkId=" + data.link_id);
-                return;
-            }
-            this.linkObject().getNameListResponse(input_val);
-        }
-    };
-
-    this.setupSessionResponse = function (input_val) {
-        this.debug_(true, this.debugInput(), "setupSessionResponse", "input_val=" + input_val);
-        var data = JSON.parse(input_val);
-        if (data) {
-            if (this.linkObject().linkId() !== data.link_id) {
-                this.abend("setupSessionResponse", "linkId=" + data.link_id);
-                return;
-            }
-            this.linkObject().setupSessionResponse(input_val);
-        }
-    };
-
-    this.setupSessionReplyResponse = function (input_val) {
-        this.debug_(true, this.debugInput(), "setupSessionReplyResponse", "input_val=" + input_val);
-        var data = JSON.parse(input_val);
-        if (data) {
-            this.linkObject().setupSessionReplyResponse(input_val);
-        }
-    };
-
-    this.putSessionDataResponse = function (input_val) {
-        this.debug_(true, this.debugInput(), "putSessionDataResponse", "input_val=" + input_val);
-        if (!input_val) {
-            return;
-        }
-
-        var data = JSON.parse(input_val);
-        if (data) {
-            this.linkObject().putSessionDataResponse(input_val);
-        }
-    };
-
-    this.getSessionDataResponse = function (input_val) {
-        this.debug_(true, this.debugInput(), "getSessionDataResponse", "input_val=" + input_val);
-        if (!input_val) {
-            return;
-        }
-
-        var data = JSON.parse(input_val);
-        if (data) {
-            this.linkObject().getSessionDataResponse(input_val);
-        }
-    };
- 
-    this.debug_ = function (debug_val, debug_val_, str1_val, str2_val) {
-        if (debug_val && debug_val_) {
-            this.logit(str1_val, str2_val);
-        }
     };
 
     this.debug = function (debug_val, str1_val, str2_val) {
