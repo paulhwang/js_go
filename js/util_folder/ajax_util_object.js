a83d0827b9c0f90918163e1ce86bf791040ebdf6
paul.hwang <phwang@gmail.com>
2016-12-10 00:58:54 +0800

    ajax

---
+++ js/util_folder/ajax_util_object.js
@@ -0,0 +1,93 @@
+function AjaxUtilObject(root_object_val, callback_func_val) {
+    "use strict";
+
+    this.init__ = function (root_object_val, callback_func_val) {
+        this.theRootObject = root_object_val;
+        this.theCallbackFunc = callback_func_val;
+        this.callbackFunc().bind(this.rootObject())("hello");
+        this.theHttpGetRequest = new XMLHttpRequest();
+        this.setupReceiveAjaxResponse();
+        this.debug(true, "init__", "");
+    };
+
+    this.objectName = function () {
+        return "AjaxUtilObject";
+    };
+
+    this.rootObject = function () {
+        return this.theRootObject;
+    };
+
+    this.callbackFunc = function () {
+        return this.theCallbackFunc;
+    };
+
+    this.ajaxRoute = function () {
+        return "/django_go/go_ajax/";
+    };
+
+    this.jsonContext = function () {
+        return "application/json; charset=utf-8";
+    }
+    this.plainTextContext = function () {
+        return "text/plain; charset=utf-8";
+    }
+
+    this.httpGetRequest = function () {
+        return this.theHttpGetRequest;
+    };
+
+    this.setupReceiveAjaxResponse = function () {
+        var this0 = this;
+        this.httpGetRequest().onreadystatechange = function() {
+            if ((this0.httpGetRequest().readyState === 4) &&
+                (this0.httpGetRequest().status === 200)) {
+                this.callbackFunc().bind(this.rootObject())(this0.httpGetRequest().responseText);
+            }
+        };
+    };
+
+    this.switchAjaxResponseData = function (json_response_val) {
+        var response = JSON.parse(json_response_val);
+        if (response.command === "setup_link") {
+            this.debug(true, "switchAjaxResponseData", "command=" + response.command + " data=" + response.data);
+            var data = JSON.parse(response.data);
+            this.rootObject().mallocLinkObject(data.my_name, data.link_id);
+        } else {
+            this.linkObject().parseAjaxResponseData(response);
+        }
+    };
+
+    this.transmitAjaxRequest = function (output_val) {
+        this.httpGetRequest().open("GET", this.ajaxRoute(), true);
+        this.httpGetRequest().setRequestHeader("X-Requested-With", "XMLHttpRequest");
+        this.httpGetRequest().setRequestHeader("Content-Type", this.jsonContext());
+        this.httpGetRequest().setRequestHeader("gorequest", output_val);
+        this.httpGetRequest().setRequestHeader("GOPACKETID", this.packetId());
+        this.incrementPacketId();
+        this.httpGetRequest().send(null);
+    };
+
+    this.debug_ = function (debug_val, debug_val_, str1_val, str2_val) {
+        if (debug_val && debug_val_) {
+            this.logit(str1_val, str2_val);
+        }
+    };
+
+    this.debug = function (debug_val, str1_val, str2_val) {
+        if (debug_val) {
+            this.logit(str1_val, str2_val);
+        }
+    };
+
+    this.logit = function (str1_val, str2_val) {
+        return LOG_IT(this.objectName() + "." + str1_val, str2_val);
+    };
+
+    this.abend = function (str1_val, str2_val) {
+        return ABEND(this.objectName() + "." + str1_val, str2_val);
+    };
+
+    this.init__(root_object_val, callback_func_val);
+}
+
